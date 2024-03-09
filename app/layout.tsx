3a63e9c39d8c45ce655149095d36bb1ee8cfcd94
Takekuni <takevadogu@outlook.jp>
2024-03-09 20:13:52 +0900

    ページ間の移動の実装

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import { Inter } from 'next/font/google';
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -8,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='{`${inter.className} antialiased`}'>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
